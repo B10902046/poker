980ac2f2117355f4ff016b3db3fc4d7054bdb99a
B10902046 <b10902046@csie.ntu.edu.tw>
2024-06-10 06:00:03 +0800

    add dqn_player

--- start_game.py
+++ start_game.py
@@ -4,16 +4,18 @@ from game.game import setup_config, start_poker
 # from agents.random_player import setup_ai as random_ai
 # from agents.console_player import setup_ai as console_ai
 from agents.dqn import setup_ai as train_ai
+from agents.dqn_player import setup_ai as dqn_ai
 
 from baseline7 import setup_ai as baseline7_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
 config.register_player(name="ta", algorithm=baseline7_ai())
-config.register_player(name="my ai", algorithm=train_ai())
+config.register_player(name="my ai", algorithm=dqn_ai())
 
 ## Play in interactive mode if uncomment
 #config.register_player(name="me", algorithm=console_ai())
-for _ in range(500):
-    game_result = start_poker(config, verbose=1)
+# for _ in range(500):
+#     game_result = start_poker(config, verbose=1)
+game_result = start_poker(config, verbose=1)
 
 print(json.dumps(game_result, indent=4))
