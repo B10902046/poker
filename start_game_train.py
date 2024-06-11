d9a736b086145192fa36971732a524ab8e8bac5f
B10902046 <b10902046@csie.ntu.edu.tw>
2024-06-12 00:23:03 +0800

    dqn modify

--- start_game_train.py
+++ start_game_train.py
@@ -1,12 +1,12 @@
 import json
 from game.game import setup_config, start_poker
-from agents.dqn import setup_ai as train_ai
+from agents.dqn_train import setup_ai as train_ai
 
 from baseline7 import setup_ai as baseline7_ai
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
 config.register_player(name="ta", algorithm=baseline7_ai())
-config.register_player(name="my ai", algorithm=dqn_ai())
+config.register_player(name="my ai", algorithm=train_ai())
 
 ## Play in interactive mode if uncomment
 #config.register_player(name="me", algorithm=console_ai())
