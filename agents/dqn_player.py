2d84519be6bed1834fa711a0121b376c349141e3
B10902046 <b10902046@csie.ntu.edu.tw>
2024-06-11 16:27:55 +0800

    debug dqn

--- agents/dqn_player.py
+++ agents/dqn_player.py
@@ -1,8 +1,8 @@
 from game.players import BasePokerPlayer
 from game.engine.card import Card
 from game.engine.hand_evaluator import HandEvaluator
-from DQN_model import DQN
-from utils import round_state_to_features
+from src.DQN_model import DQN
+from src.utils import round_state_to_features
 import random
 import torch.nn as nn
 import torch.optim as optim
@@ -18,34 +18,32 @@ class DQNPlayer(
         self.DQN.eval()
         self.num_actions = num_actions
 
-    def select_epsilon_greedy_action(self, state_feature, epsilon):
-        sample = random.random()
-        if sample > epsilon:
-            output = self.DQN(torch.Tensor(state_feature))
-            return int(torch.argmax(output).cpu())
-        else:
-            return random.randrange(self.num_actions)
-
     #  we define the logic to make an action through this method. (so this method would be the core of your AI)
     def declare_action(self, valid_actions, hole_card, round_state):
-        # valid_actions format => [fold_action_info, call_action_info, raise_action_info]
-        # print(f"valid actions: {valid_actions}")
-        # print(f"hole card: {hole_card}")
-        # print(f"round state: {round_state}")
         state_feature = round_state_to_features(valid_actions, hole_card, round_state, self.game_info)
-        
         # select the action
         try:
             raise_action_info = valid_actions[2]
             call_action_info = valid_actions[1]
             fold_action_info = valid_actions[0]
+
+            # Log the valid actions for debugging
+            print(f"Valid actions: {valid_actions}")
+
             all_actions = [[fold_action_info["action"],fold_action_info["amount"]],[call_action_info["action"],call_action_info["amount"]]]
             max_raise_amount = raise_action_info["amount"]["max"]
             min_raise_amount = raise_action_info["amount"]["min"]
             for i in range(self.num_actions-2):
                 all_actions.append([raise_action_info["action"], min_raise_amount+(max_raise_amount-min_raise_amount)*i/4])
             
+            # Log the constructed actions for debugging
+            print(f"All actions: {all_actions}")
+
             output = self.DQN(state_feature)
+
+            # Log the output tensor values for debugging
+            print(f"Output tensor values: {output}")
+            
             action_id = int(torch.argmax(output).cpu())
         except:
             print("error in action selection")
@@ -53,26 +51,13 @@ class DQNPlayer(
         return action, amount  # action returned here is sent to the poker engine
 
     def receive_game_start_message(self, game_info):
-        # game_info format => {'player_num': 2, 
-        # 'rule': {'initial_stack': 1000, 'max_round': 20, 'small_blind_amount': 5, 'ante': 0, 'blind_structure': {}},
-        # 'seats': [{'name': 'p1', 'uuid': 'ekygljctfkwbvsmxcvnqkc', 'stack': 1000, 'state': 'participating'}, {'name': 'p2', 'uuid': 'lnnrtbaxtewcewhhskwrbm', 'stack': 1000, 'state': 'participating'}]}
         self.game_info = game_info
         self.total_stack = game_info["player_num"]*game_info["rule"]["initial_stack"]
 
     def receive_round_start_message(self, round_count, hole_card, seats):
-        # round_count => int
-        # hole_card format => ['CT', 'S4']
-        # seats format => [{'name': 'p1', 'uuid': 'anhxuzprzeslfdkbelzovz', 'stack': 1065, 'state': 'participating'}, {'name': 'p2', 'uuid': 'sqmngravbajetfbqtarabx', 'stack': 920, 'state': 'participating'}]
         pass
 
     def receive_street_start_message(self, street, round_state):
-        # round_state format => {'street': 'river',
-        # 'pot': {'main': {'amount': 20}, 'side': []},
-        # 'community_card': ['C3', 'HA', 'C4', 'H3', 'D4'],
-        # 'dealer_btn': 1, 'next_player': 0, 'small_blind_pos': 0,
-        # 'big_blind_pos': 1, 'round_count': 20, 'small_blind_amount': 5,
-        # 'seats': [{'name': 'p1', 'uuid': 'anhxuzprzeslfdkbelzovz', 'stack': 1060, 'state': 'participating'}, {'name': 'p2', 'uuid': 'sqmngravbajetfbqtarabx', 'stack': 920, 'state': 'participating'}],
-        # 'action_histories': {'preflop': [{'action': 'SMALLBLIND', 'amount': 5, 'add_amount': 5, 'uuid': 'anhxuzprzeslfdkbelzovz'}, {'action': 'BIGBLIND', 'amount': 10, 'add_amount': 5, 'uuid': 'sqmngravbajetfbqtarabx'}, {'action': 'CALL', 'amount': 10, 'paid': 5, 'uuid': 'anhxuzprzeslfdkbelzovz'}, {'action': 'CALL', 'amount': 10, 'paid': 0, 'uuid': 'sqmngravbajetfbqtarabx'}], 'flop': [{'action': 'CALL', 'amount': 0, 'paid': 0, 'uuid': 'anhxuzprzeslfdkbelzovz'}, {'action': 'CALL', 'amount': 0, 'paid': 0, 'uuid': 'sqmngravbajetfbqtarabx'}], 'turn': [{'action': 'CALL', 'amount': 0, 'paid': 0, 'uuid': 'anhxuzprzeslfdkbelzovz'}, {'action': 'CALL', 'amount': 0, 'paid': 0, 'uuid': 'sqmngravbajetfbqtarabx'}], 'river': []}}
         pass
 
     def receive_game_update_message(self, action, round_state):
--- start_game_dqn.py
+++ start_game_dqn.py
@@ -1,9 +1,5 @@
 import json
 from game.game import setup_config, start_poker
-# from agents.call_player import setup_ai as call_ai
-# from agents.random_player import setup_ai as random_ai
-# from agents.console_player import setup_ai as console_ai
-from agents.dqn import setup_ai as train_ai
 from agents.dqn_player import setup_ai as dqn_ai
 
 from baseline7 import setup_ai as baseline7_ai
@@ -12,10 +8,6 @@ config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
 config.register_player(name="ta", algorithm=baseline7_ai())
 config.register_player(name="my ai", algorithm=dqn_ai())
 
-## Play in interactive mode if uncomment
-#config.register_player(name="me", algorithm=console_ai())
-# for _ in range(500):
-#     game_result = start_poker(config, verbose=1)
 game_result = start_poker(config, verbose=1)
 
 print(json.dumps(game_result, indent=4))
